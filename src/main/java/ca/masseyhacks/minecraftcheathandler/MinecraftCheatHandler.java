eced7f49f5779129052080b96dabf4b9ed38c92c
James Xu <xujames007@gmail.com>
2020-05-01 14:28:20 -0400

    use firestore/datastore

--- src/main/java/ca/masseyhacks/minecraftcheathandler/MinecraftCheatHandler.java
+++ src/main/java/ca/masseyhacks/minecraftcheathandler/MinecraftCheatHandler.java
@@ -1,74 +1,39 @@
 package ca.masseyhacks.minecraftcheathandler;
 
 import ca.masseyhacks.minecraftcheathandler.events.OnCheatDetect;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.bukkit.block.data.type.Fire;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.logging.Level;
 
 public class MinecraftCheatHandler extends JavaPlugin {
-    public Connection connection;
-    private void initDb() throws SQLException {
-        String sqlCheatCreate = "CREATE TABLE IF NOT EXISTS " + "CheatIncidents"
-                + "  (cheatUUID          VARCHAR(36),"
-                + "   mcUUID             VARCHAR(36),"
-                + "   cheatType          VARCHAR(40),"
-                + "   cheatTime          TIMESTAMP , PRIMARY KEY(`cheatUUID`))";
-        Statement stmt = connection.createStatement();
-        stmt.execute(sqlCheatCreate);
-    }
-    private boolean initMySQL(String url, String username, String password){
-        try { //We use a try catch to avoid errors, hopefully we don't get any.
-            Class.forName("com.mysql.jdbc.Driver"); //this accesses Driver in jdbc.
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            getLogger().severe("jdbc driver unavailable!");
-            return false;
-        }
-        try { //Another try catch to get any SQL errors (for example connections errors)
-            connection = DriverManager.getConnection(url, username, password);
-            //with the method getConnection() from DriverManager, we're trying to set
-            //the connection's url, username, password to the variables we made earlier and
-            //trying to get a connection at the same time. JDBC allows us to do this.
-            initDb();
-        } catch (SQLException e) { //catching errors)
-            e.printStackTrace(); //prints out SQLException errors to the console (if any)
-            return false;
-        }
-        return true;
-    }
 
-    private String buildDBURL(String host, String database){
-
-        return "jdbc:mysql://" +
-                host +
-                ":3306/" +
-                database;
-    }
     @Override
     public void onEnable(){
         //Fired when the server enables the plugin
         FileConfiguration config = this.getConfig();
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("minecraft-cheat-handler-firebase-adminsdk-v1xqd-8559bb0f17.json");
 
-        // Default config values
-        config.addDefault("mysqlHost", "127.0.0.1");
-        config.addDefault("mysqlUser", "minecraft");
-        config.addDefault("mysqlPassword", "password");
-        config.addDefault("mysqlDb", "minecraftDB");
-        config.addDefault("placeholderCacheRefreshTime", 600);
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://minecraft-cheat-handler.firebaseio.com")
+                    .build();
 
-        config.options().copyDefaults(true);
-        saveConfig();
+            FirebaseApp.initializeApp(options);
+            getLogger().info("Firebase Initialized");
 
-        if(!initMySQL(
-                buildDBURL(config.getString("mysqlHost"), config.getString("mysqlDb")),
-                config.getString("mysqlUser"),
-                config.getString("mysqlPassword"))
-        ){
-            // halt setup if MySQL cannot connect
+        } catch (Exception e) {
+            getLogger().log(Level.SEVERE,"Failed to initialize Firebase");
+
+            e.printStackTrace();
             return;
         }
 
--- src/main/java/ca/masseyhacks/minecraftcheathandler/events/OnCheatDetect.java
+++ src/main/java/ca/masseyhacks/minecraftcheathandler/events/OnCheatDetect.java
@@ -3,6 +3,12 @@ package ca.masseyhacks.minecraftcheathandler.events;
 import ca.masseyhacks.minecraftcheathandler.MinecraftCheatHandler;
 import com.gmail.olexorus.witherac.api.CheckType;
 import com.gmail.olexorus.witherac.api.ViolationEvent;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.cloud.FirestoreClient;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +17,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class OnCheatDetect implements Listener {
@@ -26,18 +34,22 @@ public class OnCheatDetect implements Listener {
         CheckType cheatType = violationEvent.getType();
         try {
             recordCheatIncident(player, cheatType);
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void recordCheatIncident(Player cheatedPlayer, CheckType checkType) throws SQLException {
+    private void recordCheatIncident(Player cheatedPlayer, CheckType checkType) {
         UUID playerUUID = cheatedPlayer.getUniqueId();
         UUID cheatUUID = UUID.randomUUID();
         String cheat = checkType.getCheckName();
-        Timestamp cheatTime = new Timestamp(Instant.now().getEpochSecond());
-        String sql = "INSERT INTO CheatIncidents VALUES (" + cheatUUID + ", " + playerUUID + ", " + cheat + ", " + cheatTime + ") ";
-        Statement stmt = plugin.connection.createStatement();
-        stmt.executeQuery(sql);
+        Firestore db = FirestoreClient.getFirestore();
+        DocumentReference docRef = db.collection("cheat-incidents").document(cheatUUID.toString());
+        Map<String, Object> data = new HashMap<>();
+        data.put("playerUUID", playerUUID.toString());
+        data.put("cheat", cheat);
+        data.put("timestamp", Instant.now().getEpochSecond());
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
     }
 }
